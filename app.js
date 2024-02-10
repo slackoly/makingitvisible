4a40e5e760548a53ad3edd04717229e0db134d1f
slackoly <sedlacko.7@osu.edu>
2024-02-10 12:32:00 -0500

    go

---
+++ app.js
@@ -0,0 +1,12 @@
+let stomata;
+let stomopen;
+//function preload() {
+  //  stomata = loadImage("./stomata_closed.png");
+    //stomopen = loadImage("./stomata_opened.png")
+//}
+
+function setup() {
+    createCanvas(900, 900)
+    background(8, 44, 26)
+
+}
