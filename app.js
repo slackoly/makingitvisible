f8bbf9e825a88d926f1235042f829e2dda7eb207
slackoly <sedlacko.7@osu.edu>
2024-02-10 13:29:13 -0500

    stomata added

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-let stomata;
-let stomopen;
-//function preload() {
-  //  stomata = loadImage("./stomata_closed.png");
-    //stomopen = loadImage("./stomata_opened.png")
-//}
+let stom;
+
+function preload() {
+  stom = loadImage('./img/stomata_closed.png');
+}
 
 function setup() {
-    createCanvas(900, 900)
-    background(8, 44, 26)
+  createCanvas(900, 900);
+  background(8, 44, 26);
+}
 
+function draw() {
+  image(stom, 0, 0, 300, 300);
+  image(stom, 0, 300, 300, 300);
+  image(stom, 0, 600, 300, 300);
+  image(stom, 300, 0, 300, 300);
+  image(stom, 300, 300, 300, 300);
+  image(stom, 300, 600, 300, 300);
+  image(stom, 600, 0, 300, 300);
+  image(stom, 600, 300, 300, 300);
+  image(stom, 600, 600, 300, 300);
 }
