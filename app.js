37236d9b4c5b4193048ebc7511f64613e057a28a
slackoly <sedlacko.7@osu.edu>
2024-02-10 15:59:00 -0500

    stomata movement

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-let stom;
+let stom1;
+let stom2;
+let stom3;
+let stom4;
+let stom5;
+let stom6;
+let stom7;
+let stom8;
+let stom9;
+let closedstom;
+let openstom;
 
 function preload() {
-  stom = loadImage('./img/stomata_closed.png');
+  stom1 = loadImage('./img/stomata_closed.png');
+  stom2 = loadImage('./img/stomata_closed.png');
+  stom3 = loadImage('./img/stomata_closed.png');
+  stom4 = loadImage('./img/stomata_closed.png');
+  stom5 = loadImage('./img/stomata_closed.png');
+  stom6 = loadImage('./img/stomata_closed.png');
+  stom7 = loadImage('./img/stomata_closed.png');
+  stom8 = loadImage('./img/stomata_closed.png');
+  stom9 = loadImage('./img/stomata_closed.png');
+  closedstom = loadImage('./img/stomata_closed.png');
+  openstom = loadImage('./img/stomata_opened.png');
 }
 
 function setup() {
@@ -10,13 +30,73 @@ function setup() {
 }
 
 function draw() {
-  image(stom, 0, 0, 300, 300);
-  image(stom, 0, 300, 300, 300);
-  image(stom, 0, 600, 300, 300);
-  image(stom, 300, 0, 300, 300);
-  image(stom, 300, 300, 300, 300);
-  image(stom, 300, 600, 300, 300);
-  image(stom, 600, 0, 300, 300);
-  image(stom, 600, 300, 300, 300);
-  image(stom, 600, 600, 300, 300);
+  image(stom1, 0, 0, 300, 300);
+  image(stom2, 0, 300, 300, 300);
+  image(stom3, 0, 600, 300, 300);
+  image(stom4, 300, 0, 300, 300);
+  image(stom5, 300, 300, 300, 300);
+  image(stom6, 300, 600, 300, 300);
+  image(stom7, 600, 0, 300, 300);
+  image(stom8, 600, 300, 300, 300);
+  image(stom9, 600, 600, 300, 300);
+}
+
+function keyPressed(){
+  if (keyCode === 81) {
+    stom1 = openstom
+  }
+  if (keyCode === 65) {
+    stom2 = openstom
+  }
+  if (keyCode === 90) {
+    stom3 = openstom
+  }
+  if (keyCode === 87) {
+    stom4 = openstom
+  }
+  if (keyCode === 83) {
+    stom5 = openstom
+  }
+  if (keyCode === 88) {
+    stom6 = openstom
+  }
+  if (keyCode === 69) {
+    stom7 = openstom
+  }
+  if (keyCode === 68) {
+    stom8 = openstom
+  }
+  if (keyCode === 67) {
+    stom9 = openstom
+  }
+}
+
+function keyReleased() {
+  if (keyCode === 81) {
+    stom1 = closedstom
+  }
+  if (keyCode === 65) {
+    stom2 = closedstom
+  }
+  if (keyCode === 90) {
+    stom3 = closedstom
+  }
+  if (keyCode === 87) {
+    stom4 = closedstom
+  }
+  if (keyCode === 83) {
+    stom5 = closedstom
+  }
+  if (keyCode === 88) {
+    stom6 = closedstom
+  }
+  if (keyCode === 69) {
+    stom7 = closedstom
+  }
+  if (keyCode === 68) {
+    stom8 = closedstom
+  }
+  if (keyCode === 67) {
+    stom9 = closedstom
+  }
 }
